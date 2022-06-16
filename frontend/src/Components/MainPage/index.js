d89b0ccda4a94e74565f67b610562d5d811558dd
FatimaKilani <fatima.keelani@gmail.com>
2022-06-16 23:15:57 +0300

    working on main page

--- frontend/src/Components/MainPage/index.js
+++ frontend/src/Components/MainPage/index.js
@@ -1,8 +1,28 @@
-import React, { useState } from "react";
+
 import "./style.css"
-
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 const MainPage=()=>{
+    const [meal, setMeal] = useState([]);
+    const { meals, page } = useSelector(
+        (state) => {
+          return {
+            meals: state.meals.meals,
+            page: state.page.page,
+          };
+        }
+      );
+
+    useEffect(() => {
+    axios
+    .get("http://localhost:5000/meals")
+    .then((result) => {
+      setMeal(result.data.result);
+    })
+    .catch((err) => {});
+}, [page]);
+
 return(
     <div className="mainpage">
 
